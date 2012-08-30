686cf6a40a212815297474142aac2da431b64798
Pat <Pat133780@gmail.com>
2012-08-29 20:50:43 -0400

    0.4
    raid, boss, areana frames done.

--- PatUI/modules/unitframes/boss.lua
+++ PatUI/modules/unitframes/boss.lua
@@ -1,9 +1,57 @@
--- Tainting UI commiting out until can figure out why.
-local T, C, L = unpack(Tukui) -- Import: T - functions, constants, variables; C - config; L - locales
-if C.unitframes.enable ~= true then return end
-local font1 = C.media.pixelfont
+local P, C, L, G = unpack(Tukui)
 
--- Bosses
+------------------------------------------------------------------------
+--	local variables
+------------------------------------------------------------------------
+
+local font = C.media.pixelfont
+local fsize = C.media.pfontsize
+
+------------------------------------------------------------------------
+-- Setup Boss Frames
+------------------------------------------------------------------------
+
 for i = 1, MAX_BOSS_FRAMES do
-	local frame = _G["TukuiBoss"..i]
+	local self = _G["TukuiBoss"..i]
+	
+	self:Size(180, 30)
+	
+	self.Health:SetHeight(30)
+	self.Health:CreateBorder()
+	self.Health:SetFrameLevel(4)
+	self.Health.bg:SetVertexColor(.25, .1, .1)
+	self.Health.bg:SetTexture(C["media"].blank)
+
+	self.Power:ClearAllPoints()
+	self.Power:Point("TOP", self.Health, "BOTTOM", 0, 4)
+	self.Power:SetHeight(8)
+	self.Power:SetWidth(143)
+	self.Power:CreateBorder()
+	self.Power:SetFrameLevel(10)
+	self.Power.colorTapping = true
+	self.Power.colorPower = true
+	
+	self.Name:SetFont(font, fsize, "MONOCHROMEOUTLINE")
+	self.Name:SetShadowOffset(0, 0)
+	
+	self.Health.value = P.SetFontString(self.Health, font, fsize, "MONOCHROMEOUTLINE")
+	self.Health.value:Point("TOPLEFT", self.Health, "TOPLEFT", 2, 0)
+
+	self.Power.value = P.SetFontString(self.Health, font, fsize, "MONOCHROMEOUTLINE")
+	self.Power.value:Point("TOPRIGHT", self.Health, "TOPRIGHT", -2, 0)
+	
+	self.Castbar.Time = P.SetFontString(self.Castbar, font, fsize, "MONOCHROMEOUTLINE")
+	self.Castbar.Time:Point("RIGHT", self.Castbar, "RIGHT", -4, 0)
+
+	self.Castbar.Text = P.SetFontString(self.Castbar, font, fsize, "MONOCHROMEOUTLINE")
+	self.Castbar.Text:Point("LEFT", self.Castbar, "LEFT", 4, 0)
+	
+	self.Buffs:SetAlpha(0)
+	self.Debuffs:SetAlpha(0)
+	
+	if i == 1 then
+		_G["TukuiBoss"..i]:SetPoint("RIGHT", UIParent, "RIGHT", -100, 0)
+	else
+		_G["TukuiBoss"..i]:SetPoint("BOTTOM", "TukuiBoss"..i - 1, "TOP", 0, 35)             
+	end
 end
--- PatUI/modules/unitframes/raid.lua
+++ PatUI/modules/unitframes/raid.lua
@@ -1,1 +1,73 @@
 local P, C, L, G = unpack(Tukui)
+
+--------------------------------------------------------------
+-- Setting up raid frames here
+--------------------------------------------------------------
+
+local font = C.media.pixelfont
+local fsize = C.media.pfontsize
+	
+P.PostUpdateRaidUnit = function(self)
+	self.panel:Kill()
+
+	self.Health:ClearAllPoints()
+	self.Health:SetAllPoints(self)
+	self.Health:CreateBorder()
+	self.Health:SetFrameLevel(10)
+	
+	self.Health.value:Point("CENTER", self.Health, 1, 13)
+	self.Health.value:SetFont(font, fsize, "MONOCHROMEOUTLINE")
+	self.Health.PostUpdate = P.PostUpdateHealthRaid
+	self.Health.bg:SetVertexColor(.25, .1, .1)
+	self.Health.value:SetShadowOffset(0, 0)
+	self.Health.bg:SetTexture(C["media"].blank)
+	
+	self.Power:ClearAllPoints()
+	self.Power:Height(5)
+	self.Power:SetWidth(P.Scale((ChatBackground:GetWidth()/ 5 ) -6) - 20)
+	self.Power:CreateBorder()
+	self.Power:Point("TOP", self.Health, "BOTTOM", 0, 4)
+	self.Power:SetStatusBarTexture(C["media"].blank)
+	self.Power:SetFrameLevel(15)
+
+	self.Name:SetParent(self.Health)
+	self.Name:ClearAllPoints()
+	self.Name:SetPoint("BOTTOM", 0, 8)
+	self.Name:SetFont(font, fsize, "MONOCHROMEOUTLINE")
+end
+
+local RaidPosition = CreateFrame("Frame")
+RaidPosition:RegisterEvent("PLAYER_LOGIN")
+RaidPosition:SetScript("OnEvent", function(self, event)
+	G.UnitFrames.RaidUnits:ClearAllPoints()
+	G.UnitFrames.RaidPets:ClearAllPoints() -- Pets can kiss my ass.
+	G.UnitFrames.RaidUnits:SetPoint("BOTTOMLEFT", ChatBackground, "TOPLEFT", 2, 6)
+end)
+
+P.RaidFrameAttributes = function()
+	return
+	"TukuiRaid",
+	nil,
+	"custom [petbattle] hide;show",
+	"oUF-initialConfigFunction", [[
+		local header = self:GetParent()
+		self:SetWidth(header:GetAttribute("initial-width"))
+		self:SetHeight(header:GetAttribute("initial-height"))
+	]],
+	"initial-width", P.Scale((ChatBackground:GetWidth()/ 5) -6),
+	"initial-height", P.Scale(30),
+	"showParty", true,
+	"showRaid", true,
+	"showPlayer", true,
+	--"showSolo", true, -- used to test raid frame without a party.
+	"xoffset", P.Scale(6),
+	"yOffset", P.Scale(-6),
+	"point", "LEFT",
+	"groupFilter", "1,2,3,4,5,6,7,8",
+	"groupingOrder", "1,2,3,4,5,6,7,8",
+	"groupBy", "GROUP",
+	"maxColumns", 8,
+	"unitsPerColumn", 5,
+	"columnSpacing", P.Scale(4),
+	"columnAnchorPoint", "TOP"
+end
