95c792b3cd3d45d1824b31815bfd0903acdfecf3
Pat <Pat133780@gmail.com>
2012-09-02 20:04:13 -0400

    don't need anymore, back in T15.12

--- PatUI/modules/skins/templates.lua
+++ PatUI/modules/skins/templates.lua
@@ -1,29 +1,5 @@
 local P, C, L, G = unpack(Tukui)
 
-local function LargeSkin()	
-	WorldMapFrame.backdrop:ClearAllPoints()
-	WorldMapFrame.backdrop:Point("TOPLEFT", WorldMapDetailFrame, "TOPLEFT", -25, 70)
-	WorldMapFrame.backdrop:Point("BOTTOMRIGHT", WorldMapDetailFrame, "BOTTOMRIGHT", 25, -30)
-end
-
-local function QuestSkin()
-	WorldMapFrame.backdrop:ClearAllPoints()
-	WorldMapFrame.backdrop:Point("TOPLEFT", WorldMapDetailFrame, "TOPLEFT", -25, 70)
-	WorldMapFrame.backdrop:Point("BOTTOMRIGHT", WorldMapDetailFrame, "BOTTOMRIGHT", 325, -235)
-end
-
-local function FixSkin()
-	if WORLDMAP_SETTINGS.size == WORLDMAP_FULLMAP_SIZE then
-		LargeSkin()
-	elseif WORLDMAP_SETTINGS.size == WORLDMAP_QUESTLIST_SIZE then
-		QuestSkin()
-	end
-end
-
-WorldMapFrame:HookScript("OnShow", FixSkin)
-hooksecurefunc("WorldMapFrame_SetFullMapView", LargeSkin)
-hooksecurefunc("WorldMapFrame_SetQuestMapView", QuestSkin)
-
 -- Overriding defualt Tukui Skin Templates here if I can.
 local frames = {
 	CharacterFrame,
