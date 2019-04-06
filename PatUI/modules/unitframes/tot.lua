cddfbc06d1452fe277bc5f0b024f84deed7aa909
Pat <PMorgan133780@gmail.com>
2019-04-06 10:50:23 -0400

    removing old UF code I no longer need on hand in my files

--- PatUI/modules/unitframes/tot.lua
+++ PatUI/modules/unitframes/tot.lua
@@ -7,34 +7,6 @@ local T, C, L = Tukui:unpack()
 local UnitFrames = T.UnitFrames
 local Noop = function() end
 
---[[local font = C.media.pixelfont
-local fsize = C.media.pfontsize
-local self = _G["TukuiTargetTarget"]
-
-self.panel:Kill()
-self.shadow:Kill()
-
-self:SetBackdrop(nil)
-self:SetBackdropColor(0, 0, 0)
-
-------------------------------------------------------------------------
--- Setup Target of Target Frames Here
-------------------------------------------------------------------------
-
-self:SetSize(110, 26)
-
-self.Health:SetHeight(23)
-self.Health:SetFrameLevel(5)
-self.Health:CreateBorder()
-
-self.Health.bg:SetTexture(C.media.normTex)
-self.Health.bg:SetVertexColor(.6, .2, .2)
-
-self.Name:SetFont(font, fsize, "MONOCHROMEOUTLINE")
-self.Name:SetShadowOffset(0, 0)
-self.Name:Point("CENTER", self.Health, "CENTER", 0, 0)
-]]--
-
 local function CreateUnits()
 	local ToT = UnitFrames.Units.TargetOfTarget
 
@@ -65,6 +37,8 @@ local function TargetOfTarget(self)
 	
 	Health.colorClass = false
 	Health.colorReaction = false
+    Health.colorTapping = false
+    Health.colorDisconnected = false
 	
 	Name:ClearAllPoints()
 	Name:SetParent(Health)
