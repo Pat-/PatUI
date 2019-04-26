a22dee9692e37b8b4668f3a3fd40b7c53c64c23b
Pat <PMorgan133780@gmail.com>
2019-04-26 15:51:29 -0400

    tooltips now change anchor depending on if bags are open or not

--- PatUI/modules/tooltip/tooltip.lua
+++ PatUI/modules/tooltip/tooltip.lua
@@ -1,22 +1,59 @@
 local T, C, L = Tukui:unpack()
 
 local Tooltips = T["Tooltips"]
+local Inventory = T["Inventory"]
 local Panels = T["Panels"]
+local Bags = Inventory["Bags"]
 
+local baseOpenAllBags = Bags.OpenAllBags
+local baseCloseAllBags = Bags.CloseAllBags
 local baseCreateAnchor = Tooltips.CreateAnchor
 
+
+-- Bags load first in Tukui
+-- so we override the bag functions only after tooltips get created
+-- This allows us to change the position of the tooltip anchor to on top of bag frames when they open
 function Tooltips:CreateAnchor()
 	-- Call the base function first
 	baseCreateAnchor(self)
 	
-	-- Then my stuff
-	local RightChat = Panels["RightChatBG"]
+	-- Make sure tooltip Anchors to where we want it before we override bag functions
 	local Anchor = Tooltips["Anchor"]
-	local Tooltip = GameTooltip
+	local RightChat = Panels["RightChatBG"]
 	
-	if C["General"]["SmallerChat"] == true then
+	if C["General"]["SmallerChat"] then
 		Anchor:ClearAllPoints()
 		Anchor:SetPoint("BOTTOMRIGHT", RightChat, "TOPRIGHT", 0, -25)
+	end
+	
+	-- Override OpenAllBags
+	function Bags:OpenAllBags()
+		-- Call the base function first
+		baseOpenAllBags(self);
+
+		-- Change the Tooltip Anchor When the Bags Get Opened
+		if C["General"]["SmallerChat"] then
+			local Anchor = Tooltips["Anchor"]
+			local Container = _G["TukuiBag"]
+			
+			Anchor:ClearAllPoints()
+			Anchor:SetPoint("BOTTOMRIGHT", Container, "TOPRIGHT", 0, 0)
+		end
+	end
+
+	-- Override CloseAllBags
+	function Bags:CloseAllBags()
+		-- Call the base function first
+		baseCloseAllBags(self);
+
+		-- Change the Tooltip Anchor Back When the Bags Get Closed
+		if C["General"]["SmallerChat"] then
+			local Anchor = Tooltips["Anchor"]
+			local RightChat = Panels["RightChatBG"]
+			
+			Anchor:ClearAllPoints()
+			Anchor:SetPoint("BOTTOMRIGHT", RightChat, "TOPRIGHT", 0, -25)
+		end
 	end
 end
 
@@ -25,6 +62,3 @@ GameTooltip:HookScript("OnShow", function(self)
 		self:Hide()
 	end
 end)
-
-
--- going to work on getting tooltip to toggle when bags are open so they're above bag frame not inside bag frame
